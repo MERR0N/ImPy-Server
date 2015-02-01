7ead3edb6a6933c953265fc740cf2aa2995617e6
MERRON <m3rr0n@gmail.com>
2015-02-01 14:33:38 +0100

    Puush fix

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os, sqlite3, md5, random
 from PIL import Image, ImageOps
 from flask import Flask, request, render_template, send_file
-from puush import puush
 
+host = "http://img.merron.ru/" # change to you server
 sqlite = sqlite3.connect('server.db3', check_same_thread=False)
 sqlcur = sqlite.cursor()
 
@@ -17,7 +17,6 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['THUMB_FOLDER'] = THUMB_FOLDER
 app.config['GALLERY'] = GALLERY
-app.register_blueprint(puush)
 
 def allowed_file(filename):
   return '.' in filename and \
@@ -119,5 +118,42 @@ def thumbnail(filename):
   if os.path.isfile(file_path):
     ImageOps.fit(Image.open(file_path), (200,200), Image.ANTIALIAS).save(os.path.join(app.config['THUMB_FOLDER'], filename))
 
+###
+    
+@app.route('/api/auth', methods=['GET', 'POST'])
+def puushauth():
+  return "0,%s,,0" % request.form['k']
+
+@app.route('/api/hist', methods=['GET', 'POST'])
+def puushhist():
+  userkey = request.form['k']
+  sqlcur.execute('SELECT * FROM "main"."pic" WHERE key = ? LIMIT 0,10',(userkey,))
+  result = sqlcur.fetchall()
+  if(result):
+    list = "0\n"
+    for row in result:
+        list += str(row[0])+","+str(datetime.datetime.fromtimestamp(int(row[4])).strftime('%Y-%m-%d %H:%M:%S'))+","+str(host)+str(row[2])+","+str(row[2])+","+str(row[3])+",0\n"
+        return list
+  return ""  
+  
+@app.route('/api/up', methods=['GET', 'POST'])
+def puushup():
+  if request.method == 'POST':
+    file = request.files['f']
+    if file and allowed_file(file.filename):
+      if request.form['k']!='public':
+        key = request.form['k']
+      else:
+        key = app.config['GALLERY']
+      filename = md5.new(str(random.random())+key).hexdigest()[:16]+"."+file.filename.rsplit('.', 1)[1]
+      sqlcur.execute('INSERT INTO "main"."pic" ("key", "name") VALUES (?, ?)',(key, filename))
+      sqlite.commit()
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      thumbnail(filename)
+      return "0,"+host+filename+",190529312,0"
+  else:
+    return render_template("upload.html")
+###    
+    
 if __name__ == "__main__":
   app.run(host='localhost',port=3000)
